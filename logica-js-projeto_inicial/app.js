d60e7f1dea3175c40839ececf050481526a1a1c1
xavierlima.ti <xavierlima.ti@gmail.com>
2024-01-24 22:25:15 -0300

    Update até módulo 4

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,6 +1,7 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 9;
 let chute = prompt('Escolha um número entre 1 e 10');
+        console.log(chute)
 //Se o chute for igual ao número secreto
 if (chute == numeroSecreto) {
          alert(`Isso aí! você descobriu o número secreto ${numeroSecreto}`);
